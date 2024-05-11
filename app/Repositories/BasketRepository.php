13782918b8af9a054f7ea4aba479e5c01c20b8a6
Ahed <ahood.as3ad10@gmail.com>
2024-05-11 20:10:42 +0300

    Add user_id t child login

---
+++ app/Repositories/BasketRepository.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Models\Category;
+
+class BasketRepository
+{
+    public function getAllCategory()
+    {
+        return Category::get();
+    }
+
+    public function getAllPaginated()
+    {
+        return Product::paginate(10);
+    }
+
+    public function getByCategory($categoryId)
+    {
+        return Product::where('category_id', $categoryId)
+            ->where('demand_state', 'approved')
+            ->paginate(10);
+    }
+
+    public function create(array $attributes)
+    {
+        return Product::create($attributes);
+    }
+
+    public function findById($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    public function update(Product $product, array $attributes)
+    {
+        $product->update($attributes);
+        return $product;
+    }
+
+    public function delete($id)
+    {
+        $product = Product::findOrFail($id);
+        return $product->delete();
+    }
+
+    public function filterProducts(array $attributes)
+    {
+        $categoryId = $attributes['category_id'] ?? null;
+        $state = $attributes['state'] ?? null;
+
+        $query = Product::query();
+
+        if ($categoryId != null) {
+            $query->where('category_id', '=', $categoryId);
+        }
+
+        if ($state != null) {
+            $query->where('state', '=', $state);
+        }
+
+        return $query->get();
+    }
+
+    public function acceptProduct($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->demand_state = 'approved';
+        $product->save();
+        return $product;
+    }
+
+    public function unacceptProduct($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->demand_state = 'rejected';
+        $product->save();
+        return $product;
+    }
+}
